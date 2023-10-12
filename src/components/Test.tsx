c683e97e468230292225222cd79074b9301fff3a
funkee <xxtrm98@protonmail.com>
2023-10-12 10:51:40 -0500

    created project folders, breakpoints, styles and added test component

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Styles/TestStyles.css";
+
+const Test = () => {
+  return (
+    <div className="test">
+      <ul>Testing</ul>
+    </div>
+  );
+};
+
+export default Test;
