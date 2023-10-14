b1b7a47027e8cc9d782967ac930f21f209889c82
funkee <xxtrm98@protonmail.com>
2023-10-14 12:57:52 -0500

    experimental get location feature

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,20 +1,35 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../Styles/TestStyles.css";
 import { GeolocationResponse } from "@/Types/types";
 
 export default function Test() {
+  const [ipAddress, setIpAddress] = useState<string>();
   const [geoLocation, setGeolocation] = useState<GeolocationResponse | null>(
     null
   );
 
   async function callApi() {
-    const res = await fetch(`/api/geolocation`);
+    const res = await fetch(
+      `/api/geolocation?ipAddress=${ipAddress ? ipAddress : null}`
+    );
     const data: GeolocationResponse = await res.json();
-    console.log(data);
+    // console.log(data);
     setGeolocation(data);
   }
-
+  useEffect(() => {
+    const fetchIp = async () => {
+      try {
+        const res = await fetch("https://api.ipify.org?format=json");
+        const data = await res.json();
+        console.log(data);
+        setIpAddress(data.ip);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchIp();
+  }, []);
   return (
     <div className="test">
       <ul>Server Component API Test</ul>
