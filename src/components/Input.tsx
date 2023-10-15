680abc87942261488603cb6d806166f403de77fe
funkee <xxtrm98@protonmail.com>
2023-10-15 12:49:32 -0500

    input styles added to form

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,23 @@
+import Image from "next/image";
 import "../Styles/InputStyles.css";
+import iconArrow from "../assets/images/icon-arrow.svg";
+
 const Input = () => {
-  return <div className="input-container">Input</div>;
+  return (
+    <div className="form-container">
+      <div className="input-container">
+        <form className="form-wrapper">
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+          />
+          <button>
+            <Image src={iconArrow} alt="icon-arrow" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
