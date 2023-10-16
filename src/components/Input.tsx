56869dcd04e3ee9be91db847fe90936e6d584635
funkee <xxtrm98@protonmail.com>
2023-10-16 12:21:34 -0500

    changed input to 'use client' and added hooks/handlefuncs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,30 @@
+"use client";
 import Image from "next/image";
 import "../Styles/InputStyles.css";
 import iconArrow from "../assets/images/icon-arrow.svg";
+import { FormEvent, useState } from "react";
 
 const Input = () => {
+  const [ipAddress, setIpAddress] = useState<string | null>();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIpAddress(e.target.value);
+  };
+
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setIpAddress(ipAddress);
+    alert(` You typed: ${ipAddress}`);
+  };
+
   return (
     <div className="form-container">
       <div className="input-container">
-        <form className="form-wrapper">
+        <form className="form-wrapper" onSubmit={handleFormSubmit}>
           <input
             type="text"
             placeholder="Search for any IP address or domain"
+            onChange={handleInputChange}
           />
           <button>
             <Image src={iconArrow} alt="icon-arrow" />
