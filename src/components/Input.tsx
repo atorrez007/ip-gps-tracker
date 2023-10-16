e4ddb55e8d1f95acb6696e06a33b545127c36c7a
funkee <xxtrm98@protonmail.com>
2023-10-16 13:43:17 -0500

    prelim validation error missing types

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,15 +6,28 @@ import { FormEvent, useState } from "react";
 
 const Input = () => {
   const [ipAddress, setIpAddress] = useState<string | null>();
-
+  const [formData, setFormData] = useState({
+    ipAddress: "",
+  });
+  const [errors, setErrors] = useState<any>({});
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIpAddress(e.target.value);
+    const { value, name } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setIpAddress(value);
   };
 
   const handleFormSubmit = (e: FormEvent) => {
     e.preventDefault();
+    // Fix type later.
+    const validationErrors: any = {};
+    if (!formData.ipAddress.trim()) {
+      validationErrors.ipAddress = "IP Address required";
+    }
+    setErrors(validationErrors);
     setIpAddress(ipAddress);
-    alert(` You typed: ${ipAddress}`);
   };
 
   return (
@@ -23,6 +36,7 @@ const Input = () => {
         <form className="form-wrapper" onSubmit={handleFormSubmit}>
           <input
             type="text"
+            name="ipAddress"
             placeholder="Search for any IP address or domain"
             onChange={handleInputChange}
           />
@@ -30,6 +44,7 @@ const Input = () => {
             <Image src={iconArrow} alt="icon-arrow" />
           </button>
         </form>
+        {errors.ipAddress && <span> {errors.ipAddress}</span>}
       </div>
     </div>
   );
