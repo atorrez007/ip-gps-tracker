6dbe839f5992e4c2224af08583cb03740dfcb17c
funkee <xxtrm98@protonmail.com>
2023-10-18 12:22:52 -0500

    testing api endpoint on client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
 import Container from "@/components/Container";
 
-async function getData() {
-  const res = await fetch("https://api.ipify.org?format=json");
-  const data = await res.json();
-  console.log(data);
-  return data;
-}
-
 export default async function Home() {
-  const data = await getData();
   return (
     <div>
-      <p>Data from server component: {data.ip}</p>
       <Container />
     </div>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -35,7 +35,7 @@ const Input = () => {
   useEffect(() => {
     const fetchIp = async () => {
       try {
-        const res = await fetch("/api/getStaticIP");
+        const res = await fetch("https://api.ipify.org?format=json");
         const data = await res.json();
         console.log(
           `data returned from api route in input component is: ${data.ip}`
