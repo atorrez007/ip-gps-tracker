c4d05eb86d88ff2bf8c7395e302dc25c1a5ecba3
funkee <xxtrm98@protonmail.com>
2023-10-30 09:57:00 -0400

    initial ip get method called

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,18 +1,23 @@
 "use client";
 import { useEffect } from "react";
 import "../Styles/ContainerStyles.css";
+import "../Styles/DashboardStyles.css";
 import ImageComponent from "./ImageComponent";
 import Map from "./Map";
 import Input from "./Input";
+import Dashboard from "./Dashboard";
 
 const Container = () => {
   useEffect(() => {
     const fetchIp = async () => {
       try {
-        const res = await fetch("https://api.ipify.org?format=json");
+        const res = await fetch(
+          "https://geo.ipify.org/api/v2/country,city?apiKey=at_dW8f9yKyopKwfeWoH3k71y3UYtyiy&ipAddress="
+        );
         const data = await res.json();
+
         console.log(
-          `data returned from api route in input component is: ${data.ip}`
+          `data returned from api route in input component is: ${data.ip} and coordinates are lat: ${data.location?.lat} and long: ${data.location?.lng} `
         );
         return data;
       } catch (err) {
@@ -22,11 +27,16 @@ const Container = () => {
     fetchIp();
   }, []);
   return (
-    <div className="container-styles">
-      <ImageComponent />
-      <section className="map-container">
-        <Map />
-      </section>
+    <div className="total-layout">
+      <div className="container-styles">
+        <ImageComponent />
+        <section className="dashboard-styles">
+          <Dashboard />
+        </section>
+        <section className="map-container">
+          <Map />
+        </section>
+      </div>
     </div>
   );
 };
